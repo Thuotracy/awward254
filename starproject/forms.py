f3cb2b0632526568c02659d6df42dc220327e1ee
tracy <tracyjacobs775@gmail.com>
2022-04-10 19:59:57 +0300

    added forms in forms.py

--- starproject/forms.py
+++ starproject/forms.py
@@ -1,5 +1,5 @@
 
-from starproject.models import Profile, Project, Review
+# from starproject.models import Profile, Project, Review
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
